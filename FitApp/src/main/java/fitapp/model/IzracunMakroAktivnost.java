ebf328a1b08e004f2055bfffbc0d1e2e60e8045a
Zoran Đukić <zorandjukic.os@gmail.com>
2021-10-18 21:34:25 +0200

    Postavljen model izračuna makronutrijenata

---
+++ FitApp/src/main/java/fitapp/model/IzracunMakroAktivnost.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fitapp.model;
+
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author PC
+ */
+public class IzracunMakroAktivnost {
+    
+    @ManyToOne
+    private DnevnikAktivnosti dnevnikAktivnosti;
+    
+    private int trajanjeAktivnosti;
+
+    public DnevnikAktivnosti getDnevnikAktivnosti() {
+        return dnevnikAktivnosti;
+    }
+
+    public void setDnevnikAktivnosti(DnevnikAktivnosti dnevnikAktivnosti) {
+        this.dnevnikAktivnosti = dnevnikAktivnosti;
+    }
+
+    public int getTrajanjeAktivnosti() {
+        return trajanjeAktivnosti;
+    }
+
+    public void setTrajanjeAktivnosti(int trajanjeAktivnosti) {
+        this.trajanjeAktivnosti = trajanjeAktivnosti;
+    }
+
+    @Override
+    public String toString() {
+        return dnevnikAktivnosti.toString();
+    }
+    
+    
+    
+}
---
+++ FitApp/src/main/java/fitapp/model/IzracunMakroHrane.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fitapp.model;
+
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author PC
+ */
+public class IzracunMakroHrane {
+
+    @ManyToOne
+    private DnevnikHrane dnevnikHrane;
+
+    private int kalorije;
+
+    private int proteini;
+
+    private int ugljikohidrati;
+
+    private int masti;
+
+    public DnevnikHrane getDnevnikHrane() {
+        return dnevnikHrane;
+    }
+
+    public void setDnevnikHrane(DnevnikHrane dnevnikHrane) {
+        this.dnevnikHrane = dnevnikHrane;
+    }
+
+    public int getKalorije() {
+        return kalorije;
+    }
+
+    public void setKalorije(int kalorije) {
+        this.kalorije = kalorije;
+    }
+
+    public int getProteini() {
+        return proteini;
+    }
+
+    public void setProteini(int proteini) {
+        this.proteini = proteini;
+    }
+
+    public int getUgljikohidrati() {
+        return ugljikohidrati;
+    }
+
+    public void setUgljikohidrati(int ugljikohidrati) {
+        this.ugljikohidrati = ugljikohidrati;
+    }
+
+    public int getMasti() {
+        return masti;
+    }
+
+    public void setMasti(int masti) {
+        this.masti = masti;
+    }
+
+    @Override
+    public String toString() {
+        return dnevnikHrane.toString();
+    }
+
+}
